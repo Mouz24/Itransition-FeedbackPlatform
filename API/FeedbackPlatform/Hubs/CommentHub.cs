cd8ded3ee73ebe959d182e7745b8e9f1cb1b1f09
Mouz24 <vadim.dontsu@mail.ru>
2023-09-10 00:50:20 +0300

    feat: add signalR hubs

---
+++ API/FeedbackPlatform/Hubs/CommentHub.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Entities.DTOs;
+using Entities.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.IdentityModel.Tokens;
+using Service.IService;
+
+namespace FeedbackPlatform.Hubs
+{
+    [Authorize(Roles = "Administrator, User")]
+    public class CommentHub : Hub
+    {
+        private readonly IMapper _mapper;
+        private readonly IServiceManager _serviceManager;
+
+        public CommentHub(IMapper mapper, IServiceManager serviceManager)
+        {
+            _mapper = mapper;
+            _serviceManager = serviceManager;
+        }
+
+        public async Task LeaveComment(CommentToLeaveDTO commentDTO)
+        {
+            if (commentDTO.Text.IsNullOrEmpty() || commentDTO.UserId == Guid.Empty || commentDTO.ReviewId == Guid.Empty)
+            {
+                return;
+            }
+
+            var comment = _mapper.Map<Comment>(commentDTO);
+
+            _serviceManager.Comment.AddComment(comment);
+            await _serviceManager.SaveAsync();
+
+            await Clients.All.SendAsync("ReceiveComment", comment);
+        }
+
+        public async Task RemoveComment(Guid id)
+        {
+            _serviceManager.Comment.RemoveComment(id);
+            await _serviceManager.SaveAsync();
+
+            await Clients.All.SendAsync("RemoveComment");
+        }
+    }
+}
---
+++ API/FeedbackPlatform/Hubs/LikeHub.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Entities.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Service;
+using Service.IService;
+using System.Data;
+
+namespace FeedbackPlatform.Hubs
+{
+    [Authorize(Roles = "Administrator, User")]
+    public class LikeHub : Hub
+    {
+        private readonly IServiceManager _serviceManager;
+        private readonly IAuthenticationManager _authenticationManager;
+        private object locker = new();
+
+        public LikeHub(IServiceManager serviceManager, IAuthenticationManager authenticationManager)
+        {
+            _serviceManager = serviceManager;
+            _authenticationManager = authenticationManager;
+        }
+
+        public async Task LikeReview(Guid reviewId, Guid userId)
+        {
+            lock(locker)
+            {
+                if (_serviceManager.LikedReview.IsReviewLikedByUser(userId, reviewId))
+                {
+                    RemoveLike(reviewId, userId).Wait();
+
+                    return;
+                }
+
+                _serviceManager.LikedReview.AddLikedReview(userId, reviewId);
+                _serviceManager.Review.LikeReview(reviewId);
+                _authenticationManager.AddLikeToUser(userId);
+            }
+
+            await _serviceManager.SaveAsync();
+
+            await Clients.All.SendAsync("LikedReview");
+        }
+
+        public async Task RemoveLike(Guid reviewId, Guid userId)
+        {
+            lock(locker)
+            {
+                _serviceManager.LikedReview.RemoveUserLike(userId, reviewId);
+                _serviceManager.Review.DislikeReview(reviewId);
+                _authenticationManager.RemoveLikeFromUser(userId);
+            }
+
+            await _serviceManager.SaveAsync();
+
+            await Clients.All.SendAsync("DislikedReview");
+        }
+    }
+}
