e5abe05fa96c02168d2f246c2de5a850bec345bd
Mouz24 <vadim.dontsu@mail.ru>
2023-09-09 20:14:30 +0300

    feat: add elasticSearch

---
+++ API/FeedbackPlatform/Extensions/ElasticSearchExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Entities.DTOs;
+using Entities.Models;
+using Nest;
+
+namespace FeedbackPlatform.Extensions
+{
+    public static class ElasticSearchExtensions
+    {
+        public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            var url = configuration["ElasticSearch:Uri"];
+            var defaultIndex = configuration["ElasticSearch:defaultIndex"];
+            var username = configuration["ElasticSearch:username"];
+            var password = configuration["ElasticSearch:password"];
+
+            var settings = new ConnectionSettings(new Uri(url))
+                .PrettyJson()
+                .DefaultIndex(defaultIndex)
+                .BasicAuthentication(username, password);
+
+            var client = new ElasticClient(settings);
+
+            services.AddSingleton<IElasticClient>(client);
+
+            CreateIndex(client, defaultIndex);
+        }
+
+        private static void CreateIndex(IElasticClient client, string indexName)
+        {
+            client.Indices.Create(indexName, i => i.Map<ReviewDTO>(x => x.AutoMap()));
+        }
+    }
+}
---
+++ API/Service/IService/ISearchService.cs
@@ -0,0 +1,15 @@
+﻿using Entities.DTOs;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.IService
+{
+    public interface ISearchService
+    {
+        Task<IEnumerable<ReviewDTO>> Search(string wordToSearch);
+    }
+}
