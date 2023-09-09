caae86f09f254c40e5e520a81d8f43b52a8574bd
Mouz24 <vadim.dontsu@mail.ru>
2023-09-09 20:44:30 +0300

    feat: add image upload service

---
+++ API/Service/IService/IImageCloudService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.IService
+{
+    public interface IImageCloudService
+    {
+        Task<string> UploadImageAsync(IFormFile uploadedFile);
+        Task DeleteImagesAsync(IEnumerable<string> imageUrls);
+    }
+}
